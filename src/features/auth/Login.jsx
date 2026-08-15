46c039b2018767d50501aa5b8b3310c25169c4af
sharifzademurad <sharifzademurad8@gmail.com>
2026-08-15 16:12:39 +0400

    task 7 tamamlandi

--- src/features/auth/Login.jsx
+++ src/features/auth/Login.jsx
@@ -1,7 +1,6 @@
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext';
-
+import { useAuth } from '../../context/AuthContext';
 const Login = () => {
   const { login } = useAuth();
   const navigate = useNavigate();
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
-import Login from '../pages/Login';
+import Login from "../features/auth/Login";
 import Dashboard from '../features/posts/Dashboard';
 import NotFound from '../pages/NotFound';
 import ProtectedRoute from './ProtectedRoute';
